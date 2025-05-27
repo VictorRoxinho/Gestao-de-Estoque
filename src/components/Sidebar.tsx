f17e7afa6086a88cc8c9db925d09af2e67cf15ba
unknown <jvcroxinho@outlook.com>
2025-05-27 11:48:57 -0300

    chore: Install Styled Components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSlack } from "@fortawesome/free-brands-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
-
 import { Link } from "react-router-dom";
 
 function Sidebar() {
@@ -16,13 +15,85 @@ function Sidebar() {
           <FontAwesomeIcon icon={faHouse} />
           <h3>Início</h3>
         </div>
+
         <div className="shortcut has-submenu">
           <h3>Vendas</h3>
           <div className="submenu">
             <Link to="/vendas/novo" className="submenu-item">
               Nova Venda
             </Link>
-            <div className="submenu-item">Histórico de Vendas</div>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Fornecedores</h3>
+          <div className="submenu">
+            <Link to="/fornecedores/novo" className="submenu-item">
+              Novo Fornecedor
+            </Link>
+            <Link to="/fornecedores/lista" className="submenu-item">
+              Lista de Fornecedores
+            </Link>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Financeiro</h3>
+          <div className="submenu">
+            <Link to="/financeiro/contas-a-pagar" className="submenu-item">
+              Contas a Pagar
+            </Link>
+            <Link to="/financeiro/contas-a-receber" className="submenu-item">
+              Contas a Receber
+            </Link>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Produtos</h3>
+          <div className="submenu">
+            <Link to="/produtos/novo" className="submenu-item">
+              Novo Produto
+            </Link>
+            <Link to="/produtos/lista" className="submenu-item">
+              Lista de Produtos
+            </Link>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Relatórios</h3>
+          <div className="submenu">
+            <Link to="/relatorios/vendas" className="submenu-item">
+              Relatório de Vendas
+            </Link>
+            <Link to="/relatorios/estoque" className="submenu-item">
+              Relatório de Estoque
+            </Link>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Cadastro</h3>
+          <div className="submenu">
+            <Link to="/cadastro/usuarios" className="submenu-item">
+              Usuários
+            </Link>
+            <Link to="/cadastro/perfis" className="submenu-item">
+              Perfis
+            </Link>
+          </div>
+        </div>
+
+        <div className="shortcut has-submenu">
+          <h3>Fiscal</h3>
+          <div className="submenu">
+            <Link to="/fiscal/nota-fiscal" className="submenu-item">
+              Nota Fiscal
+            </Link>
+            <Link to="/fiscal/nota-fiscal-eletronica" className="submenu-item">
+              Nota Fiscal Eletrônica
+            </Link>
           </div>
         </div>
       </div>
